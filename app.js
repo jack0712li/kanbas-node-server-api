80d7b80958296f3f9af79fe474fb3bc0cef04d15
18lis <jack0712li@gmail.com>
2023-11-16 21:03:23 -0500

    ready to deploy

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import CourseRoutes from "./courses/routes.js";
+import "dotenv/config";
+import session from "express-session"
 
 const app = express()
 app.use(cors());
 app.use(express.json());
-
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-
 Hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
